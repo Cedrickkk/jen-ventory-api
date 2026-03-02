a1a048363e5d62a2484c0511133905fc86d86d61
hulaanmo <inhog.cedrick@gmail.com>
2026-03-02 08:07:17 +0800

    initial commit

---
+++ src/main/java/com/jenventory/jenventoryapi/JenVentoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.jenventory.jenventoryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenVentoryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenVentoryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jenventory/jenventoryapi/JenVentoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenventory.jenventoryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenVentoryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
