3fb98f64d4459c750087535b0efaa0e6e6698bce
hulaanmo <inhog.cedrick@gmail.com>
2026-03-03 12:01:18 +0800

    refactor(security): replace RuntimeException with UsernameNotFoundException

--- src/main/java/com/jenventory/jenventoryapi/config/SecurityConfiguration.java
+++ src/main/java/com/jenventory/jenventoryapi/config/SecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -33,7 +34,7 @@ public class SecurityConfiguration {
     UserDetailsService userDetailsService() {
         return username -> userRepository.findByEmail(username)
                 .map(ApplicationUserDetails::new)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 
     @Bean
