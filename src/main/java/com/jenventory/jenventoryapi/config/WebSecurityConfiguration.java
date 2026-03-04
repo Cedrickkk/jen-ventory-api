819de83eeb977d7ec96a18274fb268b4c17fc250
hulaanmo <inhog.cedrick@gmail.com>
2026-03-04 14:15:38 +0800

    feat(security): add CORS configuration to allow requests from localhost

--- src/main/java/com/jenventory/jenventoryapi/config/WebSecurityConfiguration.java
+++ src/main/java/com/jenventory/jenventoryapi/config/WebSecurityConfiguration.java
@@ -2,8 +2,21 @@ package com.jenventory.jenventoryapi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfiguration {
+public class WebSecurityConfiguration implements WebMvcConfigurer {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
 }
