357fc2b10a6469bd465918a8d259090ca61dbb33
hulaanmo <inhog.cedrick@gmail.com>
2026-03-05 11:44:26 +0800

    refactor(customer): rename isActive column to is_active in Customer entity

--- src/main/java/com/jenventory/jenventoryapi/entity/Customer.java
+++ src/main/java/com/jenventory/jenventoryapi/entity/Customer.java
@@ -34,7 +34,7 @@ public class Customer {
     private String address;
 
     @Builder.Default
-    @Column(nullable = false)
+    @Column(name = "is_active", nullable = false)
     private boolean isActive = true;
 
     @CreationTimestamp
