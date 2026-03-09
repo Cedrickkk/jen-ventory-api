292b405cb2bd70a3be3df3ab6dd5a1ac7bc63780
hulaanmo <inhog.cedrick@gmail.com>
2026-03-09 10:36:27 +0800

    feat(stockMovement): add method to map ProductVariant and Transaction to StockMovement

--- src/main/java/com/jenventory/jenventoryapi/mapper/StockMovementMapper.java
+++ src/main/java/com/jenventory/jenventoryapi/mapper/StockMovementMapper.java
@@ -1,11 +1,26 @@
 package com.jenventory.jenventoryapi.mapper;
 
 import com.jenventory.jenventoryapi.dto.response.StockMovementResponse;
+import com.jenventory.jenventoryapi.entity.ProductVariant;
 import com.jenventory.jenventoryapi.entity.StockMovement;
+import com.jenventory.jenventoryapi.entity.Transaction;
+import com.jenventory.jenventoryapi.enums.StockMovementReason;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StockMovementMapper {
+
+    public StockMovement toEntity(
+            ProductVariant productVariant, Transaction transaction,
+            StockMovementReason reason, Integer quantityChange, String notes) {
+        return StockMovement.builder()
+                .variant(productVariant)
+                .transaction(transaction)
+                .reason(reason)
+                .quantityChange(quantityChange)
+                .notes(notes)
+                .build();
+    }
 
     public StockMovementResponse toResponse(StockMovement stockMovement) {
         return StockMovementResponse.builder()
