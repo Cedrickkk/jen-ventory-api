31c7af1fa218385f4dc105bba6d5bc27f741ba32
hulaanmo <inhog.cedrick@gmail.com>
2026-03-11 11:56:08 +0800

    feat(config): add pageable argument resolver with default settings

--- src/main/java/com/jenventory/jenventoryapi/common/config/WebConfiguration.java
+++ src/main/java/com/jenventory/jenventoryapi/common/config/WebConfiguration.java
@@ -1,13 +1,16 @@
 package com.jenventory.jenventoryapi.common.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
-@EnableWebSecurity
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class WebConfiguration implements WebMvcConfigurer {
 
@@ -19,5 +22,14 @@ public class WebConfiguration implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
+        resolver.setFallbackPageable(PageRequest.of(0, 10));
+        resolver.setMaxPageSize(100);
+        resolver.setOneIndexedParameters(true);
+        resolvers.add(resolver);
     }
 }
